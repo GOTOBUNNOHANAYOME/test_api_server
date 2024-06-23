259368b8d3b92da07dd75ccdcbb1af17fc7c98e9
test <test@Kentas-MacBook-Pro.local>
2024-06-24 00:24:01 +0900

    仮のViewの作成

---
+++ resources/views/document/create.blade.php
@@ -0,0 +1,24 @@
+@extends('index')
+
+@section('content_header')
+    <div class="col-md-6">
+        <h3>財務諸表取得</h3>
+    </div>
+@endsection
+
+@section('content')
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <input type="text" class="form-control">
+                            <button type="submit" class="btn btn-sm btn-primary">取得</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
