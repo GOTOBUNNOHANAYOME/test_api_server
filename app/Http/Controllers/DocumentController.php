19b65d194b000dc723db29be155cd3ba22c1354e
test <test@Kentas-MacBook-Pro.local>
2024-06-24 00:20:55 +0900

    コントローラー、モデルの作成

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\{
+    BalanceSheet,
+    CashFlowStatement,
+    Company,
+    Document,
+    ProfitAndLossStatement
+};
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    public function create(Request $request)
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+}
