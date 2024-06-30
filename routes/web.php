80cc33f15655e2da3e34cef0c168e868b683c55a
test <test@Kentas-MacBook-Pro.local>
2024-06-30 18:53:42 +0900

    ログイン認証用ミドルウェアの追加

--- routes/web.php
+++ routes/web.php
@@ -20,34 +20,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return to_route('image.create');
-});
-
 Route::controller(LoginHistoryController::class)->prefix('login')->group(function() {
     Route::get('/create', 'create')->name('login_history.create');
     Route::post('/', 'store')->name('login_history.store');
 });
 
-Route::controller(ImageController::class)->prefix('image')->group(function() {
-    Route::get('/create', 'create')->name('image.create');
-    Route::post('store', 'store')->name('image.store');
-    Route::get('show', 'show')->name('image.show');
-    Route::post('edit', 'edit')->name('image.edit');
+
+Route::get('/', function () {
+    return to_route('login_history.create');
 });
 
-Route::controller(FinanceController::class)->prefix('finance')->group(function() {
-    Route::get('config/create', 'createConfig')->name('finance.create_config');
-    Route::get('config/store', 'storeConfig')->name('finance.store_config');
-});
+Route::middleware(['login'])->group(function() {
+    Route::controller(ImageController::class)->prefix('image')->group(function() {
+        Route::get('/create', 'create')->name('image.create');
+        Route::post('store', 'store')->name('image.store');
+        Route::get('show', 'show')->name('image.show');
+        Route::post('edit', 'edit')->name('image.edit');
+    });
 
-Route::controller(CompanyController::class)->prefix('company')->group(function() {
-    Route::get('/create', 'create')->name('company.create');
-    Route::post('/', 'store')->name('company.store');
-    Route::get('/', 'index')->name('company.index');
-});
+    Route::controller(FinanceController::class)->prefix('finance')->group(function() {
+        Route::get('config/create', 'createConfig')->name('finance.create_config');
+        Route::get('config/store', 'storeConfig')->name('finance.store_config');
+    });
 
-Route::controller(DocumentController::class)->prefix('document')->group(function() {
-    Route::get('/create', 'create')->name('document.create');
-    Route::post('/', 'store')->name('document.store');
+    Route::controller(CompanyController::class)->prefix('company')->group(function() {
+        Route::get('/create', 'create')->name('company.create');
+        Route::post('/', 'store')->name('company.store');
+        Route::get('/', 'index')->name('company.index');
+    });
+
+    Route::controller(DocumentController::class)->prefix('document')->group(function() {
+        Route::get('/create', 'create')->name('document.create');
+        Route::post('/', 'store')->name('document.store');
+    });
 });
